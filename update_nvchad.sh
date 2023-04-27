777de8bf5396e59a8a851f803bd46322ea183793
Kushal-Chandar <kushalchandariit@gmail.com>
2023-04-27 14:32:05 +0530

    fix: update_nvchad.sh

--- update_nvchad.sh
+++ update_nvchad.sh
@@ -4,4 +4,7 @@ git remote add NvChad https://github.com/NvChad/NvChad.git
 git fetch NvChad
 git merge --allow-unrelated-histories --squash NvChad/v2.0
 git remote remove NvChad
+git checkout --ours .gitignore
+git add .gitignore
+rm -rf .github/
 git commit -m "update(NvChad): $(date +'%Y-%m-%d %H:%M:%S')"
