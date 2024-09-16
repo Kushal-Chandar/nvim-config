6d0a0d4cc47d883e6daf6329811b1ba65289aac9
kushal-chandar <kushalchandariit@gmail.com>
2024-09-16 20:46:54 +0530

    update to nvchad 2.5

--- update_nvchad.sh
+++ update_nvchad.sh
@@ -2,7 +2,7 @@
 
 git remote add NvChad https://github.com/NvChad/NvChad.git
 git fetch NvChad
-git merge --allow-unrelated-histories --squash NvChad/v2.0
+git merge --allow-unrelated-histories --squash NvChad/v2.5
 git remote remove NvChad
 git checkout --ours .gitignore
 git add .gitignore
